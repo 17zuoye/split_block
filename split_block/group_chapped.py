f9e284f17832d8c82a3b914ade554cee85ec2af1
David Chen <mvjome@gmail.com>
2014-06-30 17:38:25 +0800

    Fix maybe combined word

--- split_block/group_chapped.py
+++ split_block/group_chapped.py
@@ -8,16 +8,20 @@ class SplitBlockGroupChapped(object):
     def maybe_chapped_groups(self):
         """ Find continuous chapped SplitBlockGroup. """
         chapped_groups = []
-
         current_chapped_group = []
+
+        def is_candidate(sb1):
+            return sb1.is_letter and ((not sb1.is_regular) or \
+                                  (len(sb1) <= 5)) # maybe combined word
+
         for idx1, sb1 in enumerate(self):
-            if sb1.is_letter and (not sb1.is_regular):
+            if is_candidate(sb1):
                 current_chapped_group.append(sb1)
 
             if len(current_chapped_group) and sb1.is_blank:
                 current_chapped_group.append(sb1)
 
-            if sb1.is_other or (sb1.is_letter and sb1.is_regular) or (idx1 == (len(self) - 1 )):
+            if sb1.is_other or (sb1.is_letter and (not is_candidate(sb1))) or (idx1 == (len(self) - 1 )):
                 if len(current_chapped_group) > 2: chapped_groups.append(current_chapped_group)
                 current_chapped_group = []
 
