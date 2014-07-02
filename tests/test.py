33ae460304b5eefb741315f439de2f433b71c46b
David Chen <mvjome@gmail.com>
2014-07-02 17:02:13 +0800

    Complete test_maybe_chapped_groups

--- tests/test.py
+++ tests/test.py
@@ -7,6 +7,9 @@ from split_block import SplitBlockGroup, SplitBlock
 class TestSplitBlock(unittest.TestCase):
 
     def test_maybe_chapped_groups(self):
-        print SplitBlockGroup.extract("A. s un  B.no s e C.fa c e  D.ri c e").maybe_chapped_groups()
+        groups = SplitBlockGroup.extract("A. s un  B.no s e C.fa c e  D.ri c e").maybe_chapped_groups()
+        groups = [SplitBlockGroup(g1).concat_items() for g1 in groups]
+        self.assertEqual(groups, ['s un  ', 'no s e ', 'fa c e  ', 'ri c e'])
+
 
 if __name__ == '__main__': unittest.main()
